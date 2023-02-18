db6cd2e19543600d01544568715dcd635f8508ad
Cody Hulse <hulsecody@yahoo.com>
2023-02-17 21:24:32 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpeg$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      templateParameters: {
+        title: 'CS244 Webpack'
+      }
+    })
+  ]
+};
